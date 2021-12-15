3273a5cb95f7e3bc9eb0bcf5793978c823dfbea7
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2021-12-14 23:02:03 -0300

    Integração com banco

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,28 +4,13 @@ const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
 
-    
-
-    res.status(200).send({
-        Mensagem: 'Usando o get dentro da rota de produtos'
-    });
-});
-
-router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-    res.status(200).send({
-        Mensagem: 'Usando o get de um produto exclusivo',
-        id: id
-    });
-});
-
-router.post('/', (req, res, next) => {
-
     mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+
         conn.query(
-            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
-            [req.body.nome, req.body.preco],
-            (error, resultado, field) => {
+            'SELECT * FROM produtos;',
+            (error, resultado, fields) => {
                 conn.release();
 
                 if (error) {
@@ -35,20 +20,68 @@ router.post('/', (req, res, next) => {
                     });
                 }
 
-                res.status(201).send({
-                    Mensagem: '201 - Produto inserido com sucesso.',
-                    id_produto: resultado.insertId
+                res.status(200).send({
+                    Mensagem: '200 - Consulta realizada com sucesso.',
+                    response: resultado
                 });
             }
         )
     });
 });
 
-router.patch('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-    res.status(200).send({
-        Mensagem: 'Usando patch dentro da rota produtos para um produto expecifico',
-        id: id
+router.get('/:id_produto', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, resultado, fields) => {
+                conn.release();
+
+                if (error) {
+                    res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+
+                res.status(200).send({
+                    Mensagem: '200 - Consulta realizada com sucesso.',
+                    response: resultado
+                });
+            }
+        )
+    });
+});
+
+router.post('/', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error){ return res.status(500).send({ error: error }) }
+
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
+            [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {
+                    res.status(500).send({
+                        error: error,
+                        Mensagem: null
+                    });
+                }
+
+                res.status(201).send({
+                    Mensagem: '201 - Produto inserido com sucesso.',
+                    id_produto: resultado.insertId
+                });
+            }
+        )
     });
 });
 
